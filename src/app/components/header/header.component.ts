fb915bc2a36478645460418b61e8778d307ef1fe
Woon Him Wong <5020925-woonhwong@users.noreply.gitlab.com>
2023-04-09 02:22:07 +0100

    refine header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public activeTab = 'Latest';
   constructor() { }
 
   ngOnInit(): void {
